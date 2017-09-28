bbb5456fe4a6a6c1a2bec0a8be122362d795ab45
Eric Hartzog <AvarioDeveloper@gmail.com>
2017-09-28 15:42:43 -0400

    Cleanup tests

--- tests/server/scaling-logic-tests.js
+++ tests/server/scaling-logic-tests.js
@@ -6,7 +6,7 @@ import scalingLogic from '../../lib/scaling-logic.js';
 
 // Default scaling rules used for testing
 const scalingRules = {
-    containersMin: 1,
+    containersMin: 2,
     containersMax: 3,
     connectionsPerContainerMax: 100,
     connectionsPerContainerMin: 50,
@@ -19,59 +19,63 @@ const isEqual = (one, two) => {
 }
 
 describe('scaling logic tests', function () {
-    it('galaxy-autoscale - prevent scale down by min container', function () {
-        const status = {
-            connections: 0,
-            cpu: 0.5,
-            memory: 30,
-            containers: 1,
-            containerSize: 'compact',
-        }
+    describe('min/max container tests', function () {
+        it('prevent scale down by min container', function () {
+            const status = {
+                connections: 0,
+                cpu: 0.5,
+                memory: 30,
+                containers: 2,
+                containerSize: 'compact',
+            }
 
-        const scaleResult = scalingLogic(status, scalingRules);
+            const scaleResult = scalingLogic(status, scalingRules);
 
-        isEqual(scaleResult, 0);
-    })
+            isEqual(scaleResult, 0);
+        })
 
-    it('galaxy-autoscale - prevent scale up by max container', function () {
-        const status = {
-            connections: 10000,
-            cpu: 0.5,
-            memory: 30,
-            containers: 3,
-            containerSize: 'compact',
-        }
+        it('prevent scale up by max container', function () {
+            const status = {
+                connections: 10000,
+                cpu: 0.5,
+                memory: 30,
+                containers: 3,
+                containerSize: 'compact',
+            }
 
-        const scaleResult = scalingLogic(status, scalingRules);
+            const scaleResult = scalingLogic(status, scalingRules);
 
-        isEqual(scaleResult, 0);
-    })
+            isEqual(scaleResult, 0);
+        })
+    });
 
-    it('galaxy-autoscale - scale up by connections per container', function () {
-        const status = {
-            connections: 201,
-            cpu: 0.5,
-            memory: 30,
-            containers: 2,
-            containerSize: 'compact',
-        }
+    describe('connection per container scaling logic', function () {
+        it('scale up by connections per container', function () {
+            const status = {
+                connections: 201,
+                cpu: 0.5,
+                memory: 30,
+                containers: 2,
+                containerSize: 'compact',
+            }
 
-        const scaleResult = scalingLogic(status, scalingRules);
+            const scaleResult = scalingLogic(status, scalingRules);
 
-        isEqual(scaleResult, 1);
-    })
+            isEqual(scaleResult, 1);
+        })
 
-    it('galaxy-autoscale - scale down by connections per container', function () {
-        const status = {
-            connections: 99,
-            cpu: 0.5,
-            memory: 30,
-            containers: 2,
-            containerSize: 'compact',
-        }
+        it('scale down by connections per container', function () {
+            const status = {
+                connections: 149,
+                cpu: 0.5,
+                memory: 30,
+                containers: 3,
+                containerSize: 'compact',
+            }
 
-        const scaleResult = scalingLogic(status, scalingRules);
+            const scaleResult = scalingLogic(status, scalingRules);
 
-        isEqual(scaleResult, -1);
-    })
+            isEqual(scaleResult, -1);
+        })
+    });
 });
